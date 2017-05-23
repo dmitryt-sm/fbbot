c197fde5d86bbd0c8c45ea523a086650400c4a25
Dmitry Tsilynko <dmitry.tselinko@smava.de>
2017-05-23 19:23:33 +0300

    fixed problem with port

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.use(function(err, req, res, next) {
   res.status(500).send();
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 });
