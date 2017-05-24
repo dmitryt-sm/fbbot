679bcbbf9f70565917b57068b30b67150a7428bb
Dmitry Tsilynko <dmitry.tselinko@smava.de>
2017-05-24 10:52:57 +0300

    update app

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  VERIFY_TOKEN: 'hello_simple_fb_bot'
+  FACEBOOK_URI: 'https://graph.facebook.com/v2.6/me/messages',
+  VERIFY_TOKEN: 'hello_simple_fb_bot',
+  PAGE_ACCESS_TOKEN: 'EAAFZBLHSB844BAFSZBgWU851TYFCLQvvFrBPCWEB01rZC5ZCZBrYSDnKLxOj5PgGGy6JZCc77Y4E4R6mjXxRyGzUGHfIvhRcpofZC6Pek1epVIFwyHgUd0ENINmPbi6APmK2gt12jOt8LznNEpZAqe0Ke3ZAWBZBs4ZAvKkWqiMunbhylH2hOidU5Yi',
 };
