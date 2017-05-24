cfab454f4d9f1d00ea9fcf0ae8c88d531ddb0b8b
Dmitry Tsilynko <dmitry.tselinko@smava.de>
2017-05-24 11:57:07 +0300

    split the code into several files

---
+++ app/receiver.js
@@ -0,0 +1,25 @@
+var sendTextMessage = require('./sender');
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:",
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+    sendTextMessage(senderID, messageText);
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+module.exports = receivedMessage;
---
+++ app/sender.js
@@ -0,0 +1,39 @@
+var request = require('request');
+var config = require('../config');
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: config.FACEBOOK_URI,
+    qs: { access_token: config.PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
+
+module.exports = sendTextMessage;
